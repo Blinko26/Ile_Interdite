b8c5e9ab5f3fe8734246844dd6e470a7ff869a12
bretonem <emeric.breton31@gmail.com>
2019-05-22 09:18:42 +0200

    Merge branch 'master' of https://github.com/Blinko26/Ile_Interdite
    # Conflicts:
    #	src/ile_interdite/Aventurier.java

--- src/ile_interdite/Explorateur.java
+++ src/ile_interdite/Explorateur.java
@@ -12,26 +12,4 @@ public class Explorateur extends Aventurier {
         super(type,joueur);
         this.cPion = Pion.VERT;
     }
-    
-   // @Override
-     //   public void getMouvement(){
-       //     ArrayList<Tuile> tuilesadj = new ArrayList<Tuile>();
-         //   Ile ile = this.position.getIledescases();
-           // tuilesadj=ile.getCasesAdjacentes(position);
-//            ile.getTuilesDispoAutourJoueurClassique();
-            
-  //          for (Tuile t : ile.getTuilesdispos()) {
-    //            System.out.println("Une tuile disponible a pour emplacement : " + t.getEmplacement());
-      //      }
-            
-        //    System.out.println("Choisissez une case disponible");
-          //  Scanner sc = new Scanner(System.in);
-//            int tuilechoisie = sc.nextInt();
-            
-  //          for (Tuile t : ile.getTuilesdispos()) {
-    //            if (t.getEmplacement()==tuilechoisie) {
-      //              setPosition(ile.getTuile(tuilechoisie));
-        //        }
-          //  }
-        //}
-//}
+}
