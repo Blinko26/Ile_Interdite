7b8ef1d354a972743968853681e13c4a3460f5c5
baptiste3807 <baptistebihan38@gmail.com>
2019-05-24 14:01:38 +0200

    Merge branch 'master' of https://github.com/Blinko26/Ile_Interdite
    # Conflicts:
    #	src/ile_interdite/Explorateur.java

--- src/ile_interdite/Pilote.java
+++ src/ile_interdite/Pilote.java
@@ -1,17 +1,30 @@
 package ile_interdite;
 
 import ile_interdite.Aventurier;
+import java.util.ArrayList;
 
 public class Pilote extends Aventurier {
     
     private int ptActionSpeciale; //le pilote a un point d'action spéciale représentant sa capacité à se déplacer en hélicoptère une fois par tour
     public Pilote() {
         super(TypeAventurier.pilote,Utils.Pion.BLEU,"Heliport");
-
+        ptActionSpeciale = 1;
         
     }
     
     public int getPtActionSpeciale() { //retourne le nombre de point d'action spéciale du pilote
         return ptActionSpeciale;
+    }
+    
+    public ArrayList<Tuile> getPossibleMouvementSpecial() {
+        ArrayList<Tuile> casesDispo = new ArrayList<>();
+        if(this.getPtActionSpeciale() == 1) {
+            Tuile tu = this.getPosition();
+            Ile ile = tu.getIledescases();
+            for(Tuile t : ile.getTuilesNonInondees()) {
+                casesDispo.add(t);
+            }
+        }
+        return casesDispo;
     }
 }
