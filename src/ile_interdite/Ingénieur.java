d73a8ef18fad0a762b6e784b42a9ba753df2360c
baptiste3807 <baptistebihan38@gmail.com>
2019-06-18 16:03:15 +0200

    Merge branch 'master' of https://github.com/Blinko26/Ile_Interdite
    # Conflicts:
    #	src/Vue/VueNiveauDo.java
    #	src/ile_interdite/Application.java

--- src/ile_interdite/Ingénieur.java
+++ src/ile_interdite/Ingénieur.java
@@ -4,9 +4,21 @@ import ile_interdite.Aventurier;
 
 public class Ingénieur extends Aventurier {
     
+    private boolean aAsseche;
+    
     public Ingénieur() { 
         super(TypeAventurier.ingénieur,Utils.Pion.ROUGE,"La Porte de Bronze");
-        
+        setAsseche();
         
     }
+    
+    @Override
+    public boolean getAAsseche() {
+        return aAsseche;
+    }
+    
+    public void setAsseche() {
+        this.aAsseche = false;
+    }
+    
 }
